0c58c5b563a2abcc3bae39dc3dccb43c3d40716f
Endri Kroi <endri.kroi@gmail.com>
2023-01-31 16:21:41 +0100

    Created a basic server with several methods PUT, DELETE, GET, POST

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -1,29 +1,19 @@
 const express = require("express");
 const app = express();
-const logger = require("./logger");
-const authorize = require("./authorize");
+const auth = require("./routes/auth");
+const people = require("./routes/people");
 
-// req => middleware ==> res
-app.use([logger, authorize]);
+//static assets
+app.use(express.static("./methods-public"));
 
-// app.use("/api", logger);
-// if we add a path as above, the logger middleware will only work for the paths starting with "/api"
+//parse form data
+app.use(express.urlencoded({ extended: false }));
+//parse json
 
-app.get("/", (req, res) => {
-  res.send("Home Sabinaaaaaaa");
-});
+app.use(express.json());
 
-app.get("/about", (req, res) => {
-  res.send("About page");
-});
-
-app.get("/api/products", (req, res) => {
-  res.send("Products page");
-});
-
-app.get("/api/items", (req, res) => {
-  res.send("Items page");
-});
+app.use("/api/people", people);
+app.use("/login", auth);
 
 app.listen(5000, () => {
   console.log("Server is listening to port 5000...");
